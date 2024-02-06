c120c3358498a382dde822198cbfed0146ebb6d1
isaacenobun <76211324+isaacenobun@users.noreply.github.com>
2024-02-06 15:04:35 +0100

    Update to frontend. (Home and Product page)

--- ecommerce_website/core/views.py
+++ ecommerce_website/core/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'core/index.html')
+    context = {'title':'Home - RedStore', 'header':'header'}
+    return render(request,'core/index.html', context)
+
+def products(request):
+    context = {'title': 'Products - RedStore', 'header':'header2'}
+    return render(request,'core/products.html',context)
