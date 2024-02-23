bfd396c91cc3212bf9d979839dbd8b4d6362da0f
isaacenobun <76211324+isaacenobun@users.noreply.github.com>
2024-02-23 11:15:10 +0100

    Fix some script issues

--- ecommerce_website/core/static/js/base.js
+++ ecommerce_website/core/static/js/base.js
@@ -1,22 +1,12 @@
-// Handle image clicks
-var ProductImg = document.getElementById('product-img');
-var SmallImg = document.getElementsByClassName('small-img');
-
-for (let i = 0; i < SmallImg.length; i++) {
-    SmallImg[i].onclick = function(event) {
-        ProductImg.src = event.target.src;
-    };
-}
-
 // Handle input subtotal change
 var inputValues = document.getElementsByClassName('inputValue');
 var subTotals = document.getElementsByClassName('subTotal');
 var totalSubTotalElement = document.getElementById('subTotal-Total');
 var taxElement = document.getElementById('Tax');
-var totalElement = document.getElementById('Total')
+var totalElement = document.getElementById('Total');
 
 for (let i = 0; i < inputValues.length; i++) {
-    inputValues[i].addEventListener('input', function(event) {
+    inputValues[i].addEventListener('input', function() {
         var totalSubTotal = 0.00;
         for (let j = 0; j < inputValues.length; j++) {
             var newValue = parseFloat(inputValues[j].value);
@@ -34,24 +24,57 @@ for (let i = 0; i < inputValues.length; i++) {
 }
 
 // Handle Login and Register toggle
-var login = document.getElementById('login')
-var register = document.getElementById('register')
-var indicator = document.getElementById('Indicator')
-var loginForm = document.getElementById('LoginForm')
-var regForm = document.getElementById('RegForm')
+var login = document.getElementById('login');
+var register = document.getElementById('register');
+var indicator = document.getElementById('Indicator');
+var loginForm = document.getElementById('LoginForm');
+var regForm = document.getElementById('RegForm');
 
-login.addEventListener('click', function(){
-    indicator.style.transform = 'translateX(0px)';
-    loginForm.style.transform = 'translateX(300px)';
-    regForm.style.transform = 'translateX(300px)';
-    regForm.style.opacity = '0'
-    loginForm.style.opacity = '1'
-});
+if (login){
+    login.addEventListener('click', function(){
+        indicator.style.transform = 'translateX(0px)';
+        loginForm.style.transform = 'translateX(300px)';
+        regForm.style.transform = 'translateX(300px)';
+        regForm.style.opacity = '0'
+        loginForm.style.opacity = '1'
+    });
+}
 
-register.addEventListener('click', function(){
-    indicator.style.transform = 'translateX(100px)';
-    loginForm.style.transform = 'translateX(0)';
-    regForm.style.transform = 'translateX(0)';
-    regForm.style.opacity = '1'
-    loginForm.style.opacity = '0'
-});
+if (register){
+    register.addEventListener('click', function(){
+        indicator.style.transform = 'translateX(100px)';
+        loginForm.style.transform = 'translateX(0)';
+        regForm.style.transform = 'translateX(0)';
+        regForm.style.opacity = '1'
+        loginForm.style.opacity = '0'
+    });
+}
+
+// Handle Cart numbering
+var cartNoElement = document.getElementById('cartNo');
+var addCartElement = document.getElementById('addCart');
+var cartInputElement = document.getElementById('cartInput');
+
+if (addCartElement){
+    addCartElement.addEventListener('click',function(){
+        var cartInputValue = parseInt(cartInputElement.value);
+        cartNoElement.innerText = cartInputValue;
+        if (cartNoElement.innerText <= 0){
+            cartNoElement.style.opacity = '0'
+        } else{
+            cartNoElement.style.opacity = '0.75'
+        }
+    })
+}
+
+
+
+// Handle image clicks
+var ProductImg = document.getElementById('product-img');
+var SmallImg = document.getElementsByClassName('small-img');
+
+for (let i = 0; i < SmallImg.length; i++) {
+    SmallImg[i].onclick = function(event) {
+        ProductImg.src = event.target.src;
+    };
+}
